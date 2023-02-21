231b8e98e6d0c61d65d723e03b3742e48ffa0036
jiahong-work <124097219+jiahong-work@users.noreply.github.com>
2023-02-21 16:03:18 -0500

    Update demos.R

--- R/demos.R
+++ R/demos.R
@@ -1,11 +1,14 @@
 
-
-
-
-
-wpa_mk_new_reg <- function(.df){
+#' Make a new reg column while deleting the RegistrationDate col
+#'
+#' @param .df Data frame to add new reg col
+#' @param .date The cutoff date for new reg
+#' @return
+#' @export
+#'
+#' @examples
+ana_mk_new_reg <- function(.df,.date='2020-11-03'){
   .df |>
-    ftransform(NewReg = RegistrationDate >= lubridate::as_date()) |>
-    fselect(-RegistrationDate)
+    collapse::ftransform(NewReg = RegistrationDate >= lubridate::as_date(.date)) |>
+    collapse::fselect(-RegistrationDate)
 }
-
